c4dcfc3c11817192b88175dfa71d2ad32ba6ebe1
Gladison <gladison.ribeiro@itixti.com.br>
2022-07-23 22:37:04 -0300

    feat: adiciona pacote Intl

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_modular/flutter_modular.dart';
+import 'package:intl/date_symbol_data_local.dart';
+import 'package:intl/intl.dart';
 
 import 'app_module.dart';
 import 'app_widget.dart';
 
 void main() {
+  Intl.defaultLocale = 'pt_BR';
+  initializeDateFormatting('pt_BR', null);
   runApp(ModularApp(module: AppModule(), child: const AppWidget()));
 }
