02d518d5567159e8161f3ce5809d77e5ea216ab8
Gladison <gladison.ribeiro@itixti.com.br>
2022-07-23 22:39:05 -0300

    feat: adiciona listagem de postagens

--- lib/modules/post/presenter/posts/posts_page.dart
+++ lib/modules/post/presenter/posts/posts_page.dart
@@ -1,14 +1,63 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_modular/flutter_modular.dart';
+import 'package:gbtech_blogging_ds/gbtech_blogging_ds.dart';
 
-class PostsPage extends StatelessWidget {
+import '../widgets/post_card.dart';
+import 'posts_bloc.dart';
+import 'posts_event.dart';
+import 'posts_state.dart';
+
+class PostsPage extends StatefulWidget {
   const PostsPage({Key? key}) : super(key: key);
+
+  @override
+  State<PostsPage> createState() => _PostsPageState();
+}
+
+class _PostsPageState extends State<PostsPage> {
+  final bloc = Modular.get<PostsBloc>();
+
+  @override
+  void initState() {
+    super.initState();
+    bloc.add(PostsLoad());
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        alignment: Alignment.center,
-        child: const Text('Posts'),
+      body: PaddingLarge(
+        child: StreamBuilder(
+          stream: bloc.stream,
+          builder: (context, snapshot) {
+            final state = bloc.state;
+
+            if (state is PostsLoading) {
+              return const Center(child: CircularProgressIndicator());
+            }
+
+            if (state is PostsError) {
+              return Center(
+                child: GbLabel(
+                  'Não foi possível carregar as novidades...',
+                ),
+              );
+            }
+
+            if (state is PostsLoadSuccess) {
+              final posts = (state).posts;
+              return ListView.builder(
+                itemCount: posts.length,
+                itemBuilder: (context, index) {
+                  final post = posts[index];
+                  return PaddingSmall(child: PostCard(post: post));
+                },
+              );
+            }
+
+            return const SizedBox();
+          },
+        ),
       ),
     );
   }
