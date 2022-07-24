dacf74f1fa4a9d6ebae76550455a90cbe659596f
Gladison <gladison.ribeiro@itixti.com.br>
2022-07-24 16:47:21 -0300

    feat: adiciona atualização e remoção do post

--- lib/modules/post/presenter/widgets/post_card.dart
+++ lib/modules/post/presenter/widgets/post_card.dart
@@ -1,16 +1,156 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_modular/flutter_modular.dart';
+import 'package:gbtech_blogging/modules/auth/domain/services/auth_store.dart';
+import 'package:gbtech_blogging/modules/post/presenter/posts/posts_bloc.dart';
 import 'package:gbtech_blogging_ds/gbtech_blogging_ds.dart';
 
 import '../../../shared/utils/formart_date.dart';
 import '../../domain/entities/post.dart';
+import '../posts/posts_event.dart';
 
-class PostCard extends StatelessWidget {
+class PostCard extends StatefulWidget {
   final Post post;
 
   const PostCard({
     Key? key,
     required this.post,
   }) : super(key: key);
+
+  @override
+  State<PostCard> createState() => _PostCardState();
+}
+
+class _PostCardState extends State<PostCard> {
+  final authStore = Modular.get<AuthStore>();
+  final bloc = Modular.get<PostsBloc>();
+  final messageController = TextEditingController();
+
+  late final isPostOfCurrentUser;
+
+  @override
+  void initState() {
+    super.initState();
+    isPostOfCurrentUser =
+        widget.post.idUser == authStore.getCurrentUser().idUser;
+  }
+
+  void updateMessage(context) {
+    final message = messageController.text;
+
+    if (message.isNotEmpty) {
+      Navigator.of(context).pop();
+      bloc.add(
+        PostsChangePost(
+          idPost: widget.post.idPost!,
+          message: message,
+          currentUser: null,
+        ),
+      );
+    }
+  }
+
+  showUpdateMessageDialog(BuildContext context) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: const Text("Editar Mensagem"),
+          content: SingleChildScrollView(
+            child: Column(
+              children: [
+                GbInput(
+                  label: 'Nova mensagem',
+                  controller: messageController,
+                ),
+              ],
+            ),
+          ),
+          actions: [
+            GbTextButton(
+              onTap: () => updateMessage(context),
+              label: 'Atualizar',
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  void removeMessage(context) {
+    Navigator.of(context).pop();
+    bloc.add(
+      PostsRemovePost(
+        idPost: widget.post.idPost!,
+        currentUser: null,
+      ),
+    );
+  }
+
+  showRemoveMessageDialog(BuildContext context) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: const Text("Apagar Mensagem"),
+          content: const Text('Cuidado essa ação não pode ser revertida!'),
+          actions: [
+            GbTextButton(
+              onTap: () => removeMessage(context),
+              label: 'Confirmar',
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  Widget actions() {
+    if (!isPostOfCurrentUser) return const SizedBox();
+    return Row(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        GestureDetector(
+          child: Container(
+            alignment: Alignment.center,
+            width: 32,
+            height: 32,
+            decoration: const BoxDecoration(
+              color: buttonBackgroundColor,
+              shape: BoxShape.circle,
+            ),
+            child: const Icon(
+              Icons.edit,
+              size: 16,
+              color: backgroundColor,
+            ),
+          ),
+          onTap: () {
+            showUpdateMessageDialog(context);
+          },
+        ),
+        SpacerMedium(),
+        GestureDetector(
+          child: Container(
+            alignment: Alignment.center,
+            width: 32,
+            height: 32,
+            decoration: const BoxDecoration(
+              color: accentColor,
+              shape: BoxShape.circle,
+            ),
+            child: const Icon(
+              Icons.close,
+              size: 16,
+              color: backgroundColor,
+            ),
+          ),
+          onTap: () {
+            showRemoveMessageDialog(context);
+          },
+        ),
+      ],
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -20,29 +160,39 @@ class PostCard extends StatelessWidget {
         children: [
           Row(
             children: [
-              post.userUrlPicture != null && post.userUrlPicture!.isNotEmpty
+              widget.post.userUrlPicture != null &&
+                      widget.post.userUrlPicture!.isNotEmpty
                   ? CircleAvatar(
-                      foregroundImage: NetworkImage(post.userUrlPicture!),
+                      foregroundImage:
+                          NetworkImage(widget.post.userUrlPicture!),
                     )
                   : CircleAvatar(
                       backgroundColor: primaryColor,
-                      child: Text(
-                          (post.userName.isNotEmpty ? post.userName : ' ')
-                              .characters
-                              .first),
+                      child: Text((widget.post.userName.isNotEmpty
+                              ? widget.post.userName
+                              : ' ')
+                          .characters
+                          .first),
                     ),
               SpacerMedium(),
-              GbLabel(post.userName),
+              Expanded(
+                child: Row(
+                  children: [
+                    Expanded(child: GbLabel(widget.post.userName)),
+                    actions()
+                  ],
+                ),
+              ),
             ],
           ),
           SpacerMedium(),
-          GbText(post.message),
+          GbText(widget.post.message),
           SpacerMedium(),
           Row(
             mainAxisAlignment: MainAxisAlignment.end,
             children: [
               GbText(
-                formatDate(post.createdDate),
+                formatDate(widget.post.createdDate),
                 style: const TextStyle(fontSize: 10),
               ),
             ],
