9a99c3c55eec3dc44a9a22498c895b52ae2997f6
Gladison <gladison.ribeiro@itixti.com.br>
2022-07-25 02:41:22 -0300

    feat: define a orientação do app

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 import 'package:intl/date_symbol_data_local.dart';
 import 'package:intl/intl.dart';
@@ -6,8 +7,10 @@ import 'package:intl/intl.dart';
 import 'app_module.dart';
 import 'app_widget.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
   Intl.defaultLocale = 'pt_BR';
-  initializeDateFormatting('pt_BR', null);
+  await initializeDateFormatting('pt_BR', null);
   runApp(ModularApp(module: AppModule(), child: const AppWidget()));
 }
