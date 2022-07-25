5060cb2d1a118e4f24dfd07158f8e44e6107e581
Gladison <gladison.ribeiro@itixti.com.br>
2022-07-24 22:00:31 -0300

    feat(ds): adiciona controles aos inputs

--- packages/gbtech_blogging_ds/lib/src/components/text_fields.dart
+++ packages/gbtech_blogging_ds/lib/src/components/text_fields.dart
@@ -7,6 +7,10 @@ class GbTextField extends StatelessWidget {
   final Key? keyField;
   final void Function(String?)? onSaved;
   final TextEditingController? controller;
+  final int? maxLines;
+  final TextInputType? keyboardType;
+  final int? maxLength;
+  final TextInputAction? textInputAction;
 
   const GbTextField({
     super.key,
@@ -15,11 +19,19 @@ class GbTextField extends StatelessWidget {
     this.keyField,
     this.onSaved,
     this.controller,
+    this.maxLines = 1,
+    this.keyboardType,
+    this.maxLength,
+    this.textInputAction,
   });
 
   @override
   Widget build(BuildContext context) {
     return TextFormField(
+      keyboardType: keyboardType,
+      textInputAction: textInputAction,
+      maxLength: maxLength,
+      maxLines: maxLines,
       controller: controller,
       onSaved: onSaved,
       key: keyField,
@@ -43,6 +55,105 @@ class GbTextField extends StatelessWidget {
           borderSide: BorderSide(color: accentColor, width: 2),
         ),
       ),
+    );
+  }
+}
+
+class GbPasswordField extends StatefulWidget {
+  final String? Function(String?)? validator;
+  final String? value;
+  final Key? keyField;
+  final void Function(String?)? onSaved;
+  final TextEditingController? controller;
+  final TextInputAction? textInputAction;
+
+  const GbPasswordField({
+    super.key,
+    this.validator,
+    this.value,
+    this.keyField,
+    this.onSaved,
+    this.controller,
+    this.textInputAction,
+  });
+
+  @override
+  State<GbPasswordField> createState() => _GbPasswordFieldState();
+}
+
+class _GbPasswordFieldState extends State<GbPasswordField> {
+  bool _obscureText = true;
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  Widget _icon = Container(
+      key: const ValueKey<int>(1), child: const Icon(Icons.remove_red_eye));
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      alignment: Alignment.centerRight,
+      children: [
+        TextFormField(
+          textInputAction: widget.textInputAction,
+          obscureText: _obscureText,
+          controller: widget.controller,
+          onSaved: widget.onSaved,
+          key: widget.keyField,
+          initialValue: widget.value,
+          validator: widget.validator,
+          decoration: const InputDecoration(
+            border: OutlineInputBorder(
+              borderRadius: BorderRadius.all(Radius.circular(borderRadius)),
+              borderSide: BorderSide(color: bodyColor),
+            ),
+            enabledBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.all(Radius.circular(borderRadius)),
+              borderSide: BorderSide(color: bodyColor),
+            ),
+            focusedBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.all(Radius.circular(borderRadius)),
+              borderSide: BorderSide(color: primaryColor, width: 2),
+            ),
+            errorBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.all(Radius.circular(borderRadius)),
+              borderSide: BorderSide(color: accentColor, width: 2),
+            ),
+          ),
+        ),
+        Positioned(
+          right: 8,
+          child: GestureDetector(
+            child: Container(
+              width: 32,
+              height: 32,
+              color: Colors.transparent,
+              child: AnimatedSwitcher(
+                duration: const Duration(milliseconds: 400),
+                child: _icon,
+              ),
+            ),
+            onTap: () {
+              setState(
+                () {
+                  _obscureText = !_obscureText;
+                  _icon = _obscureText
+                      ? Container(
+                          key: const ValueKey<int>(1),
+                          child: const Icon(Icons.remove_red_eye))
+                      : Container(
+                          key: const ValueKey<int>(2),
+                          child: const Icon(Icons.remove_red_eye_outlined),
+                        );
+                },
+              );
+            },
+          ),
+        ),
+      ],
     );
   }
 }
--- packages/gbtech_blogging_ds/lib/src/patterns/input.dart
+++ packages/gbtech_blogging_ds/lib/src/patterns/input.dart
@@ -1,4 +1,4 @@
-import 'package:flutter/widgets.dart';
+import 'package:flutter/material.dart';
 
 import '../components/components.dart';
 import '../tokens/tokens.dart';
@@ -10,6 +10,10 @@ class GbInput extends StatelessWidget {
   final String? value;
   final void Function(String?)? onSaved;
   final TextEditingController? controller;
+  final int? maxLines;
+  final TextInputType? keyboardType;
+  final int? maxLength;
+  final TextInputAction? textInputAction;
 
   const GbInput({
     Key? key,
@@ -19,6 +23,10 @@ class GbInput extends StatelessWidget {
     this.keyInput,
     this.onSaved,
     this.controller,
+    this.maxLines = 1,
+    this.keyboardType,
+    this.maxLength,
+    this.textInputAction,
   }) : super(key: key);
 
   @override
@@ -33,6 +41,49 @@ class GbInput extends StatelessWidget {
           validator: validator,
           onSaved: onSaved,
           controller: controller,
+          maxLines: maxLines,
+          maxLength: maxLength,
+          keyboardType: keyboardType,
+          textInputAction: textInputAction,
+        ),
+      ],
+    );
+  }
+}
+
+class GbPasswordInput extends StatelessWidget {
+  final Key? keyInput;
+  final String label;
+  final String? Function(String?)? validator;
+  final String? value;
+  final void Function(String?)? onSaved;
+  final TextEditingController? controller;
+  final TextInputAction? textInputAction;
+
+  const GbPasswordInput({
+    Key? key,
+    required this.label,
+    this.validator,
+    this.value,
+    this.keyInput,
+    this.onSaved,
+    this.controller,
+    this.textInputAction,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        GbLabel(label),
+        SpacerSmall(),
+        GbPasswordField(
+          keyField: keyInput,
+          validator: validator,
+          onSaved: onSaved,
+          controller: controller,
+          textInputAction: textInputAction,
         ),
       ],
     );
